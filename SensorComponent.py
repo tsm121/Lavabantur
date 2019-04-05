1c9d59775130d6a7c84c5586f91569c7d7132d22
Håvard Pedersen Brandal <haavard.brandal@gmail.com>
2019-04-05 11:59:48 +0200

    Installed STMPY in virtual env

---
+++ SensorComponent.py
@@ -0,0 +1,11 @@
+import stmpy
+
+class SensorComponent:
+
+    def on_connect():
+
+    def on_message():
+
+    def __init__(self):
+
+    def stop(self):
--- SensorStateMachine.py
+++ SensorStateMachine.py
@@ -1,1 +1,9 @@
-# Implement Sensor State Machine
+import stmpy
+
+
+class SensorStateMachine:
+    def __init__(self, sensor_id, sensor):
+        print("Creating Sensor State Machine for Sensor ID " + str(sensor_id))
+
+if __name__ == "__main__":
+    stm = SensorStateMachine(1, 1)
