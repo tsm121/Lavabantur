33cbfd30d68a3f360ff6a6fc54718a7453ec5eb6
tsm121 <tsm121@gmail.com>
2019-04-25 10:08:07 +0200

    Added api and router url to django for WashingMachine

---
+++ lavabantur/backend/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include                 # add this
+from rest_framework import routers                    # add this
+from . import views                            # add this
+from django.conf.urls import url
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('washingmachine/', views.WashingMachineView.as_view({'get': 'list'})),
+]
--- lavabantur/backend/views.py
+++ lavabantur/backend/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets  # add this
+from .serializers import SerializerWashingMachine  # add this
+from .models import WashingMachineBookings  # add this
+
+
+class WashingMachineView(viewsets.ModelViewSet):  # add this
+    serializer_class = SerializerWashingMachine  # add this
+    queryset = WashingMachineBookings.objects.all()  # add this
+
