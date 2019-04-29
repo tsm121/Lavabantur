724a23e17c001e758596176a9e2af378097ffcd5
Endre Braut Medhus <Endre@dhcp-10-24-5-113.wlan.ntnu.no>
2019-04-29 11:19:17 +0200

    Make calendar View in booking

--- lavabantur/frontend/src/components/Booking.js
+++ lavabantur/frontend/src/components/Booking.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
-import { Select } from 'antd';
+import { Select, Col, Row, Button } from 'antd';
+import '../App.css';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import Router from '../Router';
+import "react-big-calendar/lib/css/react-big-calendar.css";
 const Option = Select.Option;
-
+const localizer = BigCalendar.momentLocalizer(moment);
 
 class Booking extends Component {
     constructor(props) {
         super(props);
         this.state = {
             payload: this.props.data,
-            listValues: []
+            listValues: [],
+            selectedMachine: ""
         }
 
         this.generateListName = this.generateListName.bind(this)
@@ -19,7 +25,8 @@ class Booking extends Component {
     }
 
     handleChange(value) {
-        console.log(`selected ${value}`);
+        const machine = value.split(" ")[1];
+        this.setState({selectedMachine: machine});
     }
 
     generateListName() {
@@ -36,21 +43,57 @@ class Booking extends Component {
 
     }
 
+    formatBookings(){
+        const bookings = this.state.payload;
+        const formatted = [];
+        for (var i = 0; i < bookings.length; i++){
+            if(bookings[i].washing_machine.toString() === this.state.selectedMachine){
+                const start_time = new Date(bookings[i].start_time);
+                const end_time = new Date(bookings[i].end_time);
+                const toAdd = {title: 'Booked', start: start_time, end: end_time};
+                formatted.push(toAdd);
+            }
+        }
+        return formatted;
+    }
+
     render() {
-        const {listValues} = this.state
+        const {listValues} = this.state;
+        const bookings = this.formatBookings()
+        console.log("bookings", bookings);
         return (
-            <div>
+            <div className = "booking-view-container">
                 <h1>Booking</h1>
+                <div className="buttons-container">
+                    <div className="selector-container">
 
-                <Select defaultValue="Select machine" style={{ width: "13em" }} onChange={this.handleChange}>
+                        <Select defaultValue="Select machine" style={{ width: "13em" }} onChange={this.handleChange.bind(this)}>
 
-                    {listValues.map( name => {
-                        return (
-                            <Option key={name} value={name}>{name}</Option>
-                        )
-                    })}
-                </Select>
+                            {listValues.map( name => {
+                                return (
+                                    <Option key={name} value={name}>{name}</Option>
+                                )
+                            })}
+                        </Select>
+                    </div>
+                    <div className="button-container">
+                        <Button>Add new booking</Button>
+                    </div>
+                </div>
+                <div className="booking-container">
+                    <BigCalendar 
+                        defaultView = 'week'
+                        scrollToTime = {new Date(Date.now())}
+                        localizer = {localizer}
+                        events = {bookings}
+                        startAccessor = "start"
+                        endAccessor = "end" />
+                </div>
 
+                {/*<Dayz 
+                    display= 'week'
+                    date = {moment()}
+                events = {bookings}/>*/}
             </div>
         );
     }
