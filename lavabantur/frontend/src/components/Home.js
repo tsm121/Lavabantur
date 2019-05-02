3a8f39f4043f5d78bb387aa714d97f75e141fcf8
tsm121 <tsm121@gmail.com>
2019-05-02 10:20:24 +0200

    Fixed some styling and removed login from Home

--- lavabantur/frontend/src/components/Home.js
+++ lavabantur/frontend/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import LogIn from "./LogInForm";
+import {Row, Col, Card} from "antd";
+import {Menu} from "antd/lib/menu";
 
 
 class Home extends Component {
@@ -9,21 +11,35 @@ class Home extends Component {
         return (
             <div className={"content"}>
 
-                <h1>Home</h1>
+                <div className={"home-page"}>
+                        <img id={"logo-big"} alt={"logo"} src={require('../images/logo.png')} />
+                        <h1>Lavabantur</h1>
+                </div>
 
-                <p className={"info-box"}>
+
+                <Card className={"info-box"}>
+
+                <p >
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Donec pellentesque gravida nibh.
                     Cras accumsan, libero ut commodo interdum, nibh mi semper mi, at auctor quam ipsum id lacus.
                     Donec enim nunc, lobortis eget iaculis molestie, interdum et sapien. Quisque euismod iaculis consequat.
-                    Integer egestas dolor vitae diam imperdiet, a facilisis augue sagittis. Donec semper sit amet nibh ut ornare.
-                    Maecenas laoreet tellus sed erat ornare, sit amet fringilla erat laoreet. Cras hendrerit luctus magna a mollis.
                     Cras suscipit quis urna id maximus. Donec vel blandit est. Mauris eu tincidunt metus.
                 </p>
 
-                <div id={"login-container"}>
-                    <LogIn/>
-                </div>
+                <h3>Nunc viverra</h3>
+
+                <p >
+                    Pellentesque sed risus facilisis, eleifend purus vel, efficitur velit. In ac rutrum nisl.
+                    In suscipit risus sed suscipit commodo. Nunc viverra leo at massa eleifend lobortis.
+                    Sed pellentesque velit nec lectus suscipit dapibus. Donec iaculis rhoncus malesuada.
+                    Donec varius, libero id dapibus consectetur, massa justo efficitur metus, eget gravida eros elit sit amet sem.
+                    Fusce sagittis congue nulla, ac interdum diam convallis ac. Aenean urna magna, pretium sed ultricies at, molestie sed nunc.
+                    Donec vitae sem et nisi consectetur consequat id ut turpis.
+                </p>
+
+                </Card>
+
 
             </div>
         );
